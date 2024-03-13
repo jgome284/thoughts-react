c2f50846f36f8797588db21533794e5df6f8166d
Jose M. Gomez <gomezjosem@live.com>
2024-03-13 16:09:21 +0000

    removeThought function pass as prop added to dependency to clear lint issue and future proof error

--- thoughts/src/Thought.js
+++ thoughts/src/Thought.js
@@ -15,7 +15,7 @@ export function Thought(props) {
     return () => {
       clearTimeout(timeoutId);
     };
-  }, [thought]);
+  }, [thought, removeThought]);
 
   return (
     <li className="Thought">
